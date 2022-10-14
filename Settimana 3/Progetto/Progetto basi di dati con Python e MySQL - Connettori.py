67e9bff675625b90a9118e69ab36f2c206ee3252
Filippo Testa <114301712+FilippoTesta@users.noreply.github.com>
2022-10-14 14:09:45 +0200

    Uploaded Project

---
+++ Settimana 3/Progetto/Progetto basi di dati con Python e MySQL - Connettori.py
@@ -0,0 +1,201 @@
+import mysql.connector
+
+
+def connection_database(user, password, host, database):
+    """
+    Permette di connettersi ad un database indicato, attraverso un connettore
+    :param user: Inserisci il nome dell'utente
+    :param password: Inserisci la password
+    :param host: Inserisci l'host
+    :param database: Inserisci nome del database
+    :return: Connessione al database tramite connettore
+    """
+    try:
+        conn = mysql.connector.connect(user=user, password=password, host=host,
+                                       database=database)
+        return conn
+    except mysql.connector.errors.DatabaseError as db_error:
+        print(db_error.msg)
+        return None
+
+
+def close_connection(connection):
+    """
+    Chiude la connessione
+    :param connection: Inserisci il nome della variabile data alla connessione
+    :return: Chiusura della connessione
+    """
+    connection.close()
+
+
+def execute_query(cursor, query):
+    """
+    Permette di eseguire un'interrogazione di qualsiasi tipo associata al cursore
+    :param cursor: Inserisci il cursore
+    :param query: Inserisci l'interrogazione
+    :return: Esecuzione dell'interrogazione
+    """
+    return cursor.execute(query)
+
+
+def print_result(cursor):
+    """
+    Recupera le righe nel result set dell'interrogazione e permette la visualizzazione
+    :param cursor: Inserisci il cursore
+    :return: Visualizzazione dell'interrogazione
+    """
+    for i in cursor.fetchall():
+        print(i)
+
+
+def select_from(column, table):
+    """
+    Permette di svolgere una selezione di una o più colonne da una tabella di un database
+    :param column: Inserisci nome colonna
+    :param table: Inserisci nome tabella
+    :return: Ritorna una stringa dell'interrogazione richiesta
+    """
+    return "SELECT %s FROM %s" % (column, table)
+
+
+def where(condition):
+    """
+    Permette di esprimere una condizione durante un'interrogazione
+    :param condition: Inserisci la condizione
+    :return: Ritorna una stringa dell'interrogazione richiesta
+    """
+    return " WHERE %s" % (condition)
+
+
+def groupby(column):
+    """
+    Permette di raggruppare dati identici in gruppi a partire da una colonna indicata
+    :param column: Inserisci la colonna
+    :return: Ritorna una stringa dell'interrogazione richiesta
+    """
+    return " GROUP BY %s" % (column)
+
+
+def orderby(column):
+    """
+    Permette di ordinare una tabella basandosi su una o più colonne
+    :param column: Inserisci la colonna
+    :return: Ritorna una stringa dell'interrogazione richiesta
+    """
+    return " ORDER BY %s" % (column)
+
+
+def join(tables, field):
+    """
+    Permette di combinare i valori di due tabelle attraverso una regola di confronto espressa nel parametro campo
+    :param tables: Inserisci il nome delle tabelle
+    :param field: Inserisci il valore su cui effettuare l'unione
+    :return: Ritorna una stringa dell'interrogazione richiesta
+    """
+    return " JOIN %s ON %s" % (tables, field)
+
+
+def limit(row_number):
+    """
+    Permette di limitare il risultato di un'interrogazione
+    :param row_number: Numero di righe che si desidera visualizzare nell'interrogazione
+    :return: Ritorna una stringa dell'interrogazione richiesta
+    """
+    return " LIMIT %s" % (row_number)
+
+
+conn = connection_database('root', 'root', '127.0.0.1', 'ecommerce')
+cursor = conn.cursor()
+
+#Interrogazioni con connettore MySQL
+
+#01) Prime cinque categorie di prodotto più ordinate
+
+sql = select_from('c.nome AS categoria,SUM(o.quantita) AS quantita_ordinata', 'categoria AS c') \
+      + join('prodotto AS p', 'c.cid = p.cid') + join('orpr01 AS o', 'p.pid = o.pid') + groupby('categoria') \
+      + orderby('quantita_ordinata desc') + limit('5;')
+
+execute_query(cursor, sql)
+result = print_result(cursor)
+
+
+#02) Dieci prodotti più acquistati
+
+sql2 = select_from('p.nome AS prodotto, SUM(o.quantita) AS quantita_ordinata','prodotto AS p') \
+       + join('orpr01 AS o','p.pid = o.pid') + groupby('prodotto') + orderby('quantita_ordinata desc') + limit('10;')
+
+execute_query(cursor, sql2)
+result2 = print_result(cursor)
+
+
+#03) Città da dove arrivano più ordini
+
+sql3 = select_from('COUNT(citta) AS numero_ordini, citta','indirizzo AS i') \
+       + join('ordine AS o','i.inid=o.inid') + groupby('citta') + orderby('numero_ordini desc;')
+
+execute_query(cursor,sql)
+result3 = print_result(cursor)
+
+
+#04) Numero di utenti iscritti alla newsletter
+
+sql4 = select_from('COUNT(nome)','utente') + where('nome NOT IN (') + select_from('nome','utente') \
+       + where('newsletter = 0);')
+
+execute_query(cursor,sql4)
+result4 = print_result(cursor)
+
+
+#05) Modalità di pagamento più usate
+
+sql5 = select_from('pag.nome as modalita_pagamento, COUNT(pag.nome) as quantita','pagamento pag') \
+       + join('pasp01 pas','pag.paid = pas.paid') + join('ordine o','pas.paspid = o.paspid') \
+       + groupby('modalita_pagamento') + orderby('quantita desc;')
+execute_query(cursor,sql5)
+result5 = print_result(cursor)
+
+
+#06) Marca di prodotti più ordinata
+
+sql6 = select_from('m.nome AS marca, SUM(o.quantita) AS quantita_ordinata','marca m') \
+       + join('prodotto p','m.mid = p.mid') + join('orpr01 o','p.pid = o.pid') + groupby('marca') \
+       + orderby('quantita_ordinata desc;')
+execute_query(cursor,sql6)
+result6 = print_result(cursor)
+
+
+#07) Tipo di spedizione preferita
+
+sql7 = select_from('s.nome as modalita_spedizione, COUNT(s.nome) as quantita','spedizione AS s') \
+       + join('pasp01 AS p','s.spid = p.spid') + join('ordine AS o ','p.paspid = o.paspid') \
+       + groupby('modalita_spedizione') + orderby('quantita desc;')
+execute_query(cursor,sql7)
+result7 = print_result(cursor)
+
+
+#08) Numero di ordini da parte di utenti liberi professionisti
+
+sql8 = select_from('COUNT(ordine.oid) AS numero_ordini','ordine') + join('utente','ordine.uid = utente.uid') \
+       + where('piva IS NOT NULL;')
+execute_query(cursor,sql8)
+result8 = print_result(cursor)
+
+
+#09) Prodotti più costosi ordinati
+
+sql9 = select_from('MAX(prezzo), prodotto.nome','prezzo') + join('prodotto','prezzo.pid = prodotto.pid') \
+       + join('orpr01', 'orpr01.pid = prodotto.pid') + groupby('prodotto.nome') + orderby('MAX(prezzo) desc') \
+       + limit('3;')
+execute_query(cursor, sql9)
+result9 = print_result(cursor)
+
+
+#10) Utenti che hanno effettuato più ordini
+
+sql10 = select_from\
+        ("COUNT(ordine.uid) AS numero_ordini, CONCAT(utente.nome,' ',utente.cognome) AS nome_utente", 'utente, ordine') \
+        + where('utente.uid = ordine.uid') + groupby('nome_utente') + orderby('COUNT(ordine.uid) desc;')
+execute_query(cursor, sql10)
+result10 = print_result(cursor)
+
+close = close_connection(conn)
---
+++ Settimana 3/Progetto/Progetto basi di dati con Python e MySQL2 - Pandas.py
@@ -0,0 +1,160 @@
+from sqlalchemy import create_engine
+import pandas as pd
+
+
+def connection_database(user, password, host, database):
+    """
+    Permette di connettersi ad un database indicato
+    :param user:Inserisci il nome dell'utente
+    :param password: Inserisci la password
+    :param host: Inserisci l'host
+    :param database: Inserisci nome del database
+    :return: Connessione al database
+    """
+    db_connection_str = 'mysql+pymysql://' + user + ':' + password + '@' + host + '/' + database
+    return db_connection_str
+
+#Interrogazioni su Pandas
+
+
+db_connection = create_engine(connection_database('root', 'root', '127.0.0.1', 'ecommerce'))
+
+
+#01) Prime cinque categorie di prodotto più ordinate
+prodotto = pd.read_sql("prodotto", db_connection)
+categoria = pd.read_sql("categoria", db_connection)
+orpr01 = pd.read_sql("orpr01", db_connection)
+
+df_prodotto = pd.DataFrame(prodotto)
+df_categoria = pd.DataFrame(categoria)
+df_orpr01 = pd.DataFrame(orpr01)
+
+
+df01_merged = pd.merge(df_categoria, df_prodotto, on='cid')
+df01_merged2 = pd.merge(df01_merged, df_orpr01, on='pid')
+df01_grouped = df01_merged2.groupby([('nome_x')]).sum()
+df01_grouped.sort_values(by='quantita_y', ascending=False, inplace=True)
+Top5 = df01_grouped.loc[:, ['quantita_y']]
+
+#print(Top5)
+
+
+#02) Dieci prodotti più acquistati
+
+df02 = df_orpr01.groupby('pid')['quantita', 'prezzo'].sum()
+df02.sort_values(by='quantita', ascending=False, inplace=True)
+df02.head(10).reset_index(inplace=True)
+df_merged03 = pd.merge(df02, df_prodotto, on='pid')
+Top10 = df_merged03[['nome', 'quantita_x']]
+
+#print(Top10)
+
+
+#03 Città da dove arrivano più ordini
+
+indirizzo = pd.read_sql("indirizzo", db_connection)
+ordine = pd.read_sql("ordine", db_connection)
+
+df_indirizzo = pd.DataFrame(indirizzo)
+df_ordine = pd.DataFrame(ordine)
+
+
+df03_merged = pd.merge(df_indirizzo, df_ordine, on='inid')
+df03_grouped = df03_merged.groupby([('citta')]).count()
+df03_grouped.sort_values(by='citta', ascending=False, inplace=True)
+Citta = df03_grouped[['nome']]
+
+#print(Citta)
+
+
+#04 Numero utenti iscritti alla newsletter
+
+utente = pd.read_sql("utente", db_connection)
+df_utente = pd.DataFrame(utente)
+
+df04 = df_utente.loc[df_utente['newsletter'] == 1]
+df04_count = df04.count()
+Newsletter = df04_count['newsletter']
+
+#print(Newsletter)
+
+
+#05 Modalità di pagamento più usate
+pagamento = pd.read_sql("pagamento", db_connection)
+pasp01 = pd.read_sql("pasp01", db_connection)
+
+df_pagamento = pd.DataFrame(pagamento)
+df_pasp = pd.DataFrame(pasp01)
+
+df05_merged = pd.merge(df_pagamento, df_pasp, on='paid')
+df05_merged2 = pd.merge(df05_merged, df_ordine, on='paspid')
+df05_grouped = df05_merged2.groupby([('nome')]).count()
+ModPag = df05_grouped.loc[:, ['paid']]
+
+#print(ModPag)
+
+
+#06 Marca prodotto più ordinata
+marca = pd.read_sql("marca", db_connection)
+
+df_marca = pd.DataFrame(marca)
+
+df06_merged = pd.merge(df_marca, df_prodotto, on='mid')
+df06_merged2 = pd.merge(df06_merged, df_orpr01, on='pid')
+df06_grouped = df06_merged2.groupby([('nome_x')]).sum()
+df06_grouped.sort_values(by='quantita_y', ascending=False, inplace=True)
+Marca = df06_grouped.loc[:, ['quantita_y']]
+#print(Marca)
+
+
+#07 Tipo di spedizione preferita
+spedizione = pd.read_sql("spedizione", db_connection)
+pasp01 = pd.read_sql("pasp01", db_connection)
+
+df_spedizione = pd.DataFrame(spedizione)
+df_pasp01 = pd.DataFrame(pasp01)
+
+df07_merged = pd.merge(df_spedizione, df_pasp01, on='spid')
+df07_merged2 = pd.merge(df07_merged, df_ordine, on='paspid')
+df07_grouped = df07_merged2.groupby([('nome')]).count()
+Spedizione = df07_grouped.loc[:, ['spid']]
+
+#print(Spedizione)
+
+
+#08 Numero di ordini da parte di utenti liberi professionisti
+
+df_merged_08 = pd.merge(df_ordine, df_utente, on='uid')
+df08_count= df_merged_08.count()
+pIVA = df08_count['piva']
+#print(pIVA)
+
+#print(data['c'])
+
+
+#09 Prodotto più costoso ordinato
+prezzo = pd.read_sql("prezzo", db_connection)
+
+df_prezzo = pd.DataFrame(prezzo)
+
+df09_merged = pd.merge(df_prezzo, df_prodotto, on='pid')
+df09_merged2 = pd.merge(df09_merged, df_orpr01, on='pid')
+df09_grouped = df09_merged2.groupby([('prezzo')]).max()
+df09_grouped.sort_values(by='prezzo', ascending=False, inplace=True)
+Costo = df09_grouped.loc[:, ['nome']]
+
+#print(Costo)
+
+
+#10 Utenti che hanno effettuato più ordini
+utente = pd.read_sql("utente", db_connection)
+
+df_utente = pd.DataFrame(utente)
+
+
+df10_merged = pd.merge(df_utente, df_ordine, on='uid')
+df10_grouped = df10_merged.groupby([('nome'), ('cognome')]).count()
+df10_grouped.sort_values(by='uid', ascending=False, inplace=True)
+Ordini = df10_grouped.loc[:, ['inid']]
+
+#print(Ordini)
